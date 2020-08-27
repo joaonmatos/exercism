dbcc9962c29f26bfee8d9eed9ad6c690fc80dbb9
João Nuno Matos <me@joaonmatos.com>
2020-08-27 20:47:54 +0100

    Solve rust/clock

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,43 @@
+// Copyright 2020 João Nuno Matos
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Self {
+            hours: (hours + minutes.div_euclid(60)).rem_euclid(24),
+            minutes: minutes.rem_euclid(60),
+        }
+        
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        let partial = self.minutes + minutes;
+        Self {
+            hours: (self.hours + partial.div_euclid(60)).rem_euclid(24),
+            minutes: partial.rem_euclid(60),
+        }
+    }
+}
+
+impl std::fmt::Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
+    }
+}
